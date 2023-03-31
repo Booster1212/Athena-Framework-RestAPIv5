021c20ad8d7c81c5e1b35089090fa2af62646a2c
Booster1212 <andyderking1@web.de>
2023-03-31 13:37:16 +0200

    Add example file

---
+++ server/config/rename_to_index.ts
@@ -0,0 +1,7 @@
+export const general = {
+    DISCORD_CLIENT_ID: '',
+    DISCORD_CLIENT_SECRET: '',
+    callbackURL: 'http://localhost:3000/auth/callback',
+    collection: 'api-users',
+    accessToken: '',
+};
